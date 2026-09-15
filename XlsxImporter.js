52ee03711262fc6b78d4dbe11d4c665c0630c3aa
LudoBA13 <ba130.ludovic.arnaud@banquealimentaire.org>
2026-09-15 22:52:59 +0200

    Add loadDataFromXlsx method to XlsxImporter
    Implement loadDataFromXlsx to convert XLSX files to Google Sheets, parse their
    content, and clean up the temporary files to prevent clutter.

--- XlsxImporter.js
+++ XlsxImporter.js
@@ -9,5 +9,25 @@ const XlsxImporter = {
 		// Note: Drive API Advanced service must be enabled in the project
 		const file = Drive.Files.insert(resource, blob, { convert: true });
 		return SpreadsheetApp.openById(file.id);
+	},
+
+	loadDataFromXlsx: function(base64Data, fileName)
+	{
+		const tempSpreadsheet = this.convertXlsxToSheets(base64Data, fileName);
+		const data = {};
+		try
+		{
+			const sheets = tempSpreadsheet.getSheets();
+			for (let i = 0; i < sheets.length; i++)
+			{
+				const sheet = sheets[i];
+				data[sheet.getName()] = sheet.getDataRange().getValues();
+			}
+		}
+		finally
+		{
+			DriveApp.getFileById(tempSpreadsheet.getId()).setTrashed(true);
+		}
+		return data;
 	}
 };
